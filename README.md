97227a5a1d9ae4dc143767e2b7d55434de34135e
Thomas Walker Lynch <thomas.lynch@reasoningtechnology.com>
2020-06-29 16:05:49 +0200

    prep work for inclusive upper bound

--- README.md
+++ README.md
@@ -42,6 +42,56 @@ Hence the name, Tape Machine 2x.
   array is stored in.  If the allocation's byte length is larger than the array's byte
   length, there will be some allocated memory that goes unused.
 
+  Then according to our nomenclature we start with the container, then name the element type,
+  give the subscript, then give the type.
+  
+  ```
+          array_byte_0_pt
+          memory_byte_0_pt
+  ```
+
+   `array_byte_0_pt` is a pointer to the first byte in an array.  `memory_byte_n_pt` is a pointer
+   to the last byte in memory.  Where the container context is clear we leave that off.  In 
+   most of our array implementation the container is clearly the array.  Hence,
+   
+  ```
+          element_n_pt
+  ```
+
+   in the array implementation `element_n_pt` would be a pointer to the last element in the array.
+   
+   When the subscript is given as a variable we are asking about that variable's value. Hence,
+   
+   ```
+       array_byte_n
+       byte_n
+   ```
+   for a 16 byte array these both would have the value 15.  In the second one the array container is implied.
+   Suppose this same array had elements that were 4 byte integers, then 
+   
+   ```
+       array_element_n
+   ```
+   Would have the value of 3.
+   
+   Note that to get the nth byte we would have
+
+   ```
+       byte_0_pt[byte_n]
+   ```
+   
+   Note and to get the nth element:
+   
+   ```
+       ((int *)byte_0_pt)[element_n]
+   ```
+   
+   Or even:
+   
+   ```
+       *((int *)byte_0_pt element_n)
+   ```
+
 ## Allocation Algorithm
 
   We are implementing a dynamic array, i.e. one that may grow in length when elements
@@ -81,37 +131,72 @@ Hence the name, Tape Machine 2x.
 
   Note the example usage in the test files.
 
-## Small Memories
+## Eliminating `allocation_np1_pt` and Inclusive Bounds for Small Memories
 
-  At the time of this writing, attotw, 2020-06-27T14:57:19Z, our array implementaiton uses
-  `byte_0_pt` to locate the first byte in the array, and `byte_np1_pt` (np1 == n + 1) to
-  bound data in the array.  It uses `allocation_np1_pt` to bound the allocation. A nice
-  property that this model has is that when `byte_np1_pt` is equal to `byte_0_pt` we know
-  the array is zero length.  Though our name `byte_0_pt` is a misnomer in that case, as
-  there is no byte 0.
-
-  I would like to change this implementation so that this dynamic array implementation may
-  work on a page of memory, on a very small machine, or even over a small memory buffer.
-
-  So as to model this small problem, suppose there are 16 bytes of memory, that a pointer
-  is 4 bits. Say an element is a byte. With so little memory we do not want to waste a
-  single byte. Suppose further that we do not allow overflows when performing arithmetic.
-  Hence adding one to a pointer can be a problem.
-
-  Note that the approach attotw violates these small memory representation constraints,
-  so I would like to change the implementation.
+  At the time of this writing, attotw, (2020-06-27T14:57:19Z in case you want to look it
+  up on github), our array implementaiton uses an exclusive upper bound convention, where
+  the pointer `byte_0_pt` locates the first byte in the array, and the pointer
+  `byte_np1_pt`, where np1 stands for n plus 1, is used to bound the data in the array,
+  and `allocation_np1_pt` is used to bound the allocation.  We also use a doubling algorithm
+  on expansion, and hystersis on collapse with a 1/4 size threshold.
   
-  Suppose instead we used `byte_0_pt` to locate the base, and then keep `byte_length_data`
-  and `byte_length_allocation` instead of `byte_np1_pt` and `allocation_np1_pt`. This also
-  makes it possible to represent a zero length array, but it also violates our small memory
-  representation constraints.
+  With this dynamic array algorithm the `allocation_np1_pt` value is always a simple power
+  of two, or a bound on the memory as a whole.  If we collapsed immediately instead of
+  using hysterisis and the 1/4 threshold rule, we would not need the `allocation_np1_pt`
+  pointer at all, because it would point to either the end of memory or to the top of the
+  binade of `byte_np1_pt` value.  Eg.  when `byte_np1_pt` is 4, 5, or 6, 7, `allocation_np1_pt`
+  would be 8.  I.e. 100, 101, 110, 111 would be bound by 1000. Etc.
+
+  A nice property of 'np1' bounding pointers the ability to represent zero length objects.
+  Hence when `byte_np1_pt` is equal to `byte_0_pt` we know the array is zero length.
+  Philosophically, in the computation theory sense, the zero length array is an
+  interesting object.  With the zero length array, the name of the pointer we called
+  `byte_0_pt` becomes a misnomer, because there is no byte 0 in an empty array.  Another
+  thing that is a bit funny is that though the array exists, and we know it exists because
+  the base pointer is not null, it has no members. It is not entirely clear what that
+  means for an array with no members to 'exist'.  But this next thing is the most
+  consequential:
+
+  For a memory between `2^(n-1) + 1` to `2^n` in size, we must implement n bit pointers.
+  However for the end case of `2^n` sized memory, to be able to use exclusive upper bound
+  pointers, we will require pointers of length n+1 rather than length n. This will affect
+  all registers, buffers, and programs that use pointers. And through such a larger unsigned
+  integer at n+1 bits could encode twice as many values as an integer with n bits, we will
+  use only one of those encodings.
   
-  Suppose then, that we use inclusive bounds on our data and allocation intervals.  We
-  would lose the ability to represent a zero length array.  Instead the smallest array
-  would have a minimum of one element when `byte_0_pt` equals `byte_n_pt`.  Though then,
-  at least, the term `byte_0_pt` would always be semantically accurate.  The array existing
-  would also correspond to the array have non-zero length, so we do not have to ask the
-  question what the existence of a zero length array means.
+  OK, suppose I change this implementation for a dynamic array and use an inclusive upper
+  bound instead of an exclusive upper bound.  I.e. instead of saying that the array is
+  bound by the pointer `byte_np1_pt`, let us bound the array with `byte_n_pt. Such an
+  implementation will be useful when we want to limit our array allocation to a single
+  page of memory, say in an implementation that uses mapped pages, if we have a special
+  machine with a small memory, or if we were designing our own hardware and wanted to
+  implement an array over a memory buffer.
+
+  For purposes of visualization of end case you might imagine that there are 16 bytes of
+  memory, that a pointer is 4 bits, an that array elements are 1 or two bytes. With such a
+  small memory we do not want to waste a single byte. Suppose further that we do not allow
+  overflows when performing arithmetic.  I.e. we do not want to build into the
+  implementation dependencies on the specific behavior of what happens when we add 1 to
+  1111. (It would be good to test this code with such a model, but that has not been done
+  yet.)
+
+  With this new implementation all arithmetic for the array is done in a manner that it
+  will never overflow or underflow and all values may be held within the representation.
+  However, the C library routines which we need, `malloc` and `sizeof`, do not conform.
+  Maybe someday we will go new versions of those, but for now, as a hack, I just created
+  `#define` versions of the routines we wish for, and then these expand out to the
+  the C library versions. The type `size_t` is actually the address type for the machine
+  C is targetting, so for clarity I aliased that to `address_t`.
+
+  When we use inclusive bounds on our data, we lose the ability to represent a zero length
+  array.  Instead the smallest array, where `byte_0_pt` equals `byte_n_pt`, will have one
+  element in it. In trade for this we will no longer have to debate the meaning of an
+  array existing though it has no elements in it, and `byte_0_pt` will indeed alawys point
+  to byte 0.  If our array is being used for something like a FIFO we will have to use an
+  external flag to represent when the FIFO is empty.  But again, we find this to be
+  conceptually clean because that is a special case that changes the behavior of pulling
+  from the FIFO.  Had we had an array that could be empty, would have found ourselves
+  always checking for that case separately, i.e. using the array in an anomolous manner
+  before using it as an array. Arguably then, the new approach is cleaner.
   
-  However, with this approach, is it always possible to do the arithmetic for mainting the
-  array without overflows in a clean manner?  Lets find out ..
+
--- TM2x.h
+++ TM2x.h
@@ -1,71 +1,114 @@
 #ifndef TM2x_LIB_H
 #define TM2x_LIB_H
-#include <stdlib.h> // byte_length_t
+#include <stdlib.h> // size_t
 #include <string.h> // memcpy
 #include <stdbool.h>
 #include <assert.h>
+#include <limits.h>
 
-#define byte_length_t size_t
-#define byte_length_of sizeof
 
 //--------------------------------------------------------------------------------
 // global stuff
 //
 
-  // referenced in TM2x_init and TM2x_data_dealloc, used to count the number of
-  // dynamic arrays that have allocations on the heap. Useful for debug, easily removed.
-  extern byte_length_t TM2x_outstanding_cnt;
-
-  // inlining the interface
+  // inlining style for the interface. 'extern inline' will result in the smallest
+  // executable.  'static inline' is the simplest for development as there is only
+  // one point that needs to be edited.  Giving no value leads to the simplest debugging but
+  // will be slower.  The compiler can ignore requests to inline, and it looks like gcc
+  // is doing so when the debugging flag is turned on and optimization is turned off.
   //#define TM2x_F_PREFIX
   //#define TM2x_F_PREFIX extern inline
   #define TM2x_F_PREFIX static inline
 
-  // We guarantee that the array byte_length is always a non-negative integer multiple of the
-  // element_byte_length.  
+  // To make inclusive bounds really work we would need some new C library functions.
+  // Here are some mockups of those new functions. These provide the correct interface
+  // but still call the C library.
   //
-    #define TM2x_INITIAL_ALLOCATION_ELEMENT_LENGTH 4 
+    TM2x_F_PREFIX void *mallocn(n){
+      void *pt = malloc(n+1); // n+1 might overflow
+      assert(pt); // we can not use null pointers as flags if memory 0 is to be addressable
+      return pt;
+    }
+    // without the assert
+    // #define mallocn(n) malloc(n+1)
 
+    // the n+1 could overflow:
+    #define memcpyn(dst ,src ,n) memcpy(dst ,src ,n+1) 
+
+    // sizeof will overflow if asking for the size of a type that takes up the entire buffer,
+    // so instead we just ask for the last byte offset into the buffer.
+    #define byte_n_of(type) sizeof(type)-1
+
+  // 'size'is an ambiguous term. size_t is the native address type on a machine, so we
+  // will just call it that.
+  #define address_t size_t
+
+  // This is a debug feature. To keep the heap clean, all initialized arrays must be
+  // TM2x_data_dalloc'ed.  This counter is incremented upon initialization of an array,
+  // and it is decremented upon TM2x_data_dalloc.
+  extern address_t TM2x_outstanding_cnt;
+
+  // The max index for an element in the initial allocation. The array allocation might
+  // later expand and contract, but will never be smaller than this.  A value of n = 3
+  // is to say that initially we will have an allocation of 4 elements indexed 0 to 3.
+  // The smallest that this value may be set to is 0, for a one element initial allocation.
+  #define TM2x_INITIAL_ALLOCATION_ELEMENT_N 3
+
+  // what is a maximum virtual pointer value?  'ULONG_MAX' might be wishful thinking,
+  // as OS's do not alway provide a full sized virtual memory space. Note, all pointers
+  // to memory originate from the OS, and the OS might not give us predictive control.
+  #define TM2x_MEMORY_BYTE_N_PT ULONG_MAX 
 
 //--------------------------------------------------------------------------------
-// doubling array instance
-//
+// dynamic array instance 
+//    
   typedef struct {
-    char *byte_0_pt; // byte 0 of both the array and the allocation
-    char *byte_np1_pt; // one byte beyond the end of the array
-    char *allocation_byte_np1_pt; // one byte beyond the end of the allocation
-    byte_length_t element_byte_length;
+    char *base_pt;
+    char *byte_n_pt;
+    address_t element_byte_n; // length of an element - 1
   } TM2x;
 
-//--------------------------------------------------------------------------------
+//------------------------o--------------------------------------------------------
 // alloc/init/dealloc
 //
-  TM2x_F_PREFIX TM2x *TM2x_init(TM2x *dap ,byte_length_t element_byte_length){
-    TM2x_outstanding_cnt++;
-    dap->element_byte_length = element_byte_length;
-    byte_length_t allocation_byte_length = TM2x_INITIAL_ALLOCATION_ELEMENT_LENGTH * element_byte_length;
-    dap->byte_0_pt = malloc(allocation_byte_length);
-    assert(dap->byte_0_pt);
-    dap->allocation_byte_np1_pt = dap->byte_0_pt + allocation_byte_length;
-    dap->byte_np1_pt = dap->byte_0_pt; 
-    return dap;
+  // changing units in array size computation, (a_n + 1) (b_n + 1) - 1
+  TM2x_F_PREFIX address_t TM2x_mulns(address_t a_n , address_t b_n){
+    return a_n * b_n + a_n + b_n;
   }
-  #define TM2x_Make(da ,type) TM2x TM2x_ ## da ,*da; da = &TM2x_ ## da; TM2x_init(da ,byte_length_of(type));
 
-  // after data_dallocation, the TM2x may be re-initialized and used again
+  TM2x_F_PREFIX address_t TM2x_INITIAL_ALLOCATION_BYTE_N(address_t element_byte_n){
+    return mulns(TM2x_INITIAL_ALLOCATION_ELEMENT_N ,element_byte_n);
+  }
+
+  // After initialiazation there is one data element, and an allocation 
+  // for TM2x_INITIAL_ALLOCATION_ELEMENT_N more elements.
+  // need to add limit checks, because init could fail to fit, and return a bool
+  TM2x_F_PREFIX bool TM2x_init(TM2x *dap ,address_t element_byte_n){
+    TM2x_outstanding_cnt++;
+    dap->element_byte_n = element_byte_n;
+    address_t allocation_byte_n = TM2x_INITIAL_ALLOCATION_BYTE_N(element_byte_n);
+    dap->base_pt = mallocn(allocation_byte_n);
+    assert(dap->base_pt);
+    dap->allocation_byte_n_pt = dap->byte_0_pt + allocation_byte_n;
+    dap->byte_n_pt = dap->base_pt;  
+    return true;
+  }
+  #define TM2x_Make(da ,type) TM2x TM2x_ ## da ,*da; da = &TM2x_ ## da; TM2x_init(da ,byte_length_of(type)-1);
+
+  // after data_deallocation, the TM2x may be re-initialized and used again
   TM2x_F_PREFIX void TM2x_data_dealloc(TM2x *dap){
     free(dap->byte_0_pt);
     TM2x_outstanding_cnt--;
   }
 
-  TM2x_F_PREFIX byte_length_t TM2x_outstanding(TM2x *dap){
+  TM2x_F_PREFIX address_t TM2x_outstanding(TM2x *dap){
     return TM2x_outstanding_cnt;
   }
 
   // for dynammic allocation of TM2xs:
-  TM2x_F_PREFIX TM2x *TM2x_alloc(byte_length_t element_byte_length){
-    TM2x *dap = malloc(byte_length_of(TM2x));
-    TM2x_init(dap ,element_byte_length);
+  TM2x_F_PREFIX TM2x *TM2x_alloc(address_t element_byte_n){
+    TM2x *dap = mallocn(byte_n_of(TM2x));
+    TM2x_init(dap ,element_byte_n);
     return dap;
   }
   TM2x_F_PREFIX void TM2x_dealloc(TM2x *dap){
@@ -74,68 +117,127 @@
   }
 
 //--------------------------------------------------------------------------------
-// adjectives
+// instance interface use this interface for the rest of the code. Do not refer to the
+// instance directly.  This frees us to change the instance implementation later.
 //
-  TM2x_F_PREFIX byte_length_t TM2x_element_byte_length(TM2x *dap){
-    return dap->element_byte_length;
+
+  // base pointers
+  TM2x_F_PREFIX address_t TM2x_byte_0_pt(TM2x *dap){
+    return dap->base_pt;
   }
-  TM2x_F_PREFIX byte_length_t TM2x_byte_length(TM2x *dap){
-    return dap->byte_np1_pt - dap->byte_0_pt;
+  TM2x_F_PREFIX address_t TM2x_element_0_pt(TM2x *dap){
+    return dap->base_pt;
   }
-  TM2x_F_PREFIX bool TM2x_empty(TM2x *dap){
-    return dap->byte_np1_pt == dap->byte_0_pt;
+  TM2x_F_PREFIX address_t TM2x_allocation_byte_0_pt(TM2x *dap){
+    return dap->base_pt;
   }
-  TM2x_F_PREFIX byte_length_t TM2x_allocation_byte_length(TM2x *dap){
-    return dap->allocation_byte_np1_pt - dap->byte_0_pt;
+
+  // max offsets
+  TM2x_F_PREFIX address_t TM2x_element_byte_n(TM2x *dap){
+    return dap->element_byte_n;
   }
+  TM2x_F_PREFIX address_t TM2x_byte_n(TM2x *dap){
+    return dap->byte_n_pt - dap->byte_0_pt;
+  }
+  TM2x_F_PREFIX address_t TM2x_allocation_byte_n(TM2x *dap){
+    return dap->allocation_byte_n_pt - dap->byte_0_pt;
+  }
+  // element_byte_n + 1 must be representable:
+  TM2x_F_PREFIX address_t TM2x_element_n(TM2x *dap){
+    return dap->byte_n/(dap->element_byte_n + 1);
+  }
+
+  // nth pointers
+  TM2x_F_PREFIX address_t TM2x_byte_n_pt(TM2x *dap){
+    return dap->byte_n_pt;
+  }
+  TM2x_F_PREFIX address_t TM2x_element_n_pt(TM2x *dap){
+    return dap->byte_n_pt - dap->element_byte_n;
+  }
+  TM2x_F_PREFIX address_t TM2x_allocation_byte_n_pt(TM2x *dap){
+    return dap->allocation_byte_n_pt;
+  }
+
+  // A private method.  Makes a new allocation, moves the data over, sets the array pointers
+  // to the new allocation. The caller is under contract to assure the data fits in the
+  // new allocation.
+  TM2x_F_PREFIX void __TM2x_adjust(TM2x *dap ,address_t new_allocation_n){
+    char *new_data_allocation_pt = mallocn( new_allocation_n );
+    address_t byte_n = TM2x_byte_n(dap);
+    memcpyn( new_data_allocation_pt ,TM2x_byte_0_pt(dap) ,byte_n);
+    free(TM2x_allocation_byte_0_pt(dap));
+    dap->base_pt = new_data_allocation_pt;
+    dap->byte_n_pt = new_data_allocation_pt + byte_n;
+    dap->allocation_byte_n_pt = new_data_allocation_pt + new_allocation_n;
+  }
+
+
 
 //--------------------------------------------------------------------------------
-// change byte_length
+// change allocation size
 //
-  // private
-  TM2x_F_PREFIX void __TM2x_adjust(TM2x *dap ,byte_length_t new_allocation_byte_length){
-    byte_length_t data_byte_length = TM2x_byte_length(dap);
-    char *new_data_allocation_pt = malloc( new_allocation_byte_length );
-    assert( new_data_allocation_pt );
-    memcpy( new_data_allocation_pt ,dap->byte_0_pt ,data_byte_length );
-    free(dap->byte_0_pt);
-    dap->byte_0_pt = new_data_allocation_pt;
-    dap->byte_np1_pt = new_data_allocation_pt + data_byte_length;
-    dap->allocation_byte_np1_pt = new_data_allocation_pt + new_allocation_byte_length;
+  TM2x_F_PREFIX address_t TM2x_expand(TM2x *dap){
+    address_t max_expansion = TM2x_MEMORY_BYTE_N_PT - TM2x_allocation_byte_n_pt(dap);
+    if( max_expansion == 0 ) return 0;
+    // we now know that TM2x_alloction_byte_n is less than memory_byte_n
+    address_t desired_expansion = TM2x_allocation_byte_n(dap) + 1; // we want to double the allocation, m1 == -1
+    if( desired_expansion > max_expansion ) desired_expansion = max_expansion;
+    __TM2x_adjust(dap ,TM2x_allocation_byte_n(dap) + desired_expansion);
+    return desired_expansion;
   }
-  TM2x_F_PREFIX void TM2x_expand(TM2x *dap){
-    byte_length_t new_allocation_byte_length = TM2x_allocation_byte_length(dap) << 1;
-    __TM2x_adjust(dap ,new_allocation_byte_length);
-  }
-  TM2x_F_PREFIX void TM2x_collapse(TM2x *dap){
-    byte_length_t initial_allocation_byte_length = TM2x_INITIAL_ALLOCATION_ELEMENT_LENGTH * TM2x_element_byte_length(dap);
-    byte_length_t new_allocation_byte_length = TM2x_allocation_byte_length(dap) >> 1;
-    if( new_allocation_byte_length < initial_allocation_byte_length || dap->byte_0_pt + new_allocation_byte_length < dap->byte_np1_pt )
-      return;
-    __TM2x_adjust(dap ,new_allocation_byte_length);
+  TM2x_F_PREFIX bool TM2x_collapse(TM2x *dap){
+    if( TM2x_byte_n(dap) <= TM2x_allocation_byte_n(dap) << 2 ){ // then collapse
+      address_t initial_allocation_byte_n = TM2x_INITIAL_ALLOCATION_BYTE_N(TM2x_element_byte_n(dap));
+      if( TM2x_allocation_byte_n(dap) == iniital_allocation_byte_n ) return false;
+      address_t collapse_to = TM2x_allocation_byte_n(dap) << 1;
+      if( collapse_to < initial_allocation_byte_n) collapse_to = initial_allocation_byte_n;
+      __TM2x_adjust(dap ,collapse_to);
+      return true;
+    }
+    return false;
   }
 
-  TM2x_F_PREFIX char *TM2x_push_alloc(TM2x *dap ,byte_length_t count){
-    byte_length_t push_byte_length = count * TM2x_element_byte_length(dap);
-    while( dap->byte_np1_pt + push_byte_length > dap->allocation_byte_np1_pt ) TM2x_expand(dap);
-    char *element_pt = dap->byte_np1_pt;
-    dap->byte_np1_pt += push_byte_length;
-    return element_pt;
+  // the original array top should be saved before calling this.
+  // if doubling expansion does not work, 
+  TM2x_F_PREFIX bool TM2x_push_alloc(TM2x *dap ,address_t requested_expansion_element_n){
+    address_t data_expansion_byte_n = TM2x_mulns(element_n ,TM2x_element_byte_n(dap));
+    
+
+
+    address_t expansion, total_expansion=0;
+    do{
+      expansion = TM2x_expand(dap);
+      total_expansion += expansion;
+    }while( expansion != 0 && total_expansion <= requested_expansion_byte_n );
+    return expansion != 0;
   }
-  TM2x_F_PREFIX char *TM2x_push_write(TM2x *dap ,void *src_element_pt){
-    char *dst_element_pt = TM2x_push_alloc(dap ,1);
-    memcpy(dst_element_pt, src_element_pt, TM2x_element_byte_length(dap));
-    return dst_element_pt;
+  TM2x_F_PREFIX bool TM2x_push_write(TM2x *dap ,void *src_element_pt){
+    if
+      ( 
+       TM2x_MEMORY_BYTE_N_PT - TM2x_allocation_byte_n_pt(dap) <= TM2x_element_byte_n(dap)
+       ||
+       (TM2x_byte_n_pt(dap) + TM2x_element_byte_n(dap) + 1) >= TM2x_allocation_byte_n_pt(dap)
+       &&
+       !TM2x_push_alloc(dap ,0)
+        )
+      return false;
+
+    memcpyn(TM2x_element_n_pt(dap), src_element_pt, TM2x_element_byte_n(dap));
+    return true;
   }
 
+  // the read will always succeed, but the pop might not
   TM2x_F_PREFIX bool TM2x_pop_read(TM2x *dap ,void *dst_element_pt){
+    memcpyn(dst_element_pt, TM2x_element_n_pt(dap) ,TM2x_element_byte_n(dap));
+    if( TM2x_byte_0_pt(dap) == TM2x_byte_n_pt(dap) ) return false;
+    
     if( TM2x_empty(dap) ) return false;
-    char *src_element_pt = dap->byte_np1_pt - TM2x_element_byte_length(dap);
-    if( dst_element_pt ) memcpy(dst_element_pt, src_element_pt, TM2x_element_byte_length(dap));
+    char *src_element_pt = dap->byte_n_pt - TM2x_element_byte_n(dap);
+    if( dst_element_pt ) 
     dap->byte_np1_pt = src_element_pt;
 
     // if the array fits in 1/4 of the allocation, then collapse the alloction by 1/2
-    byte_length_t allocation_byte_length_collapse_threshold = TM2x_allocation_byte_length(dap) >> 2;
+    address_t allocation_byte_length_collapse_threshold = TM2x_allocation_byte_length(dap) >> 2;
     if( TM2x_byte_length(dap) <= allocation_byte_length_collapse_threshold ) TM2x_collapse(dap);
 
     return true;
@@ -147,10 +249,10 @@
 //--------------------------------------------------------------------------------
 // indexing
 //
-  TM2x_F_PREFIX byte_length_t TM2x_least_index(TM2x *dap){
+  TM2x_F_PREFIX address_t TM2x_least_index(TM2x *dap){
     return 0;
   }
-  TM2x_F_PREFIX byte_length_t TM2x_greatest_index(TM2x *dap){
+  TM2x_F_PREFIX address_t TM2x_greatest_index(TM2x *dap){
     return TM2x_byte_length(dap) / TM2x_element_byte_length(dap);
   }
   TM2x_F_PREFIX void *TM2x_at_least_index_pt(TM2x *dap){
@@ -164,11 +266,11 @@
   }
 
   // returns pointer to element at given index
-  TM2x_F_PREFIX void *TM2x_at_index_pt(TM2x *dap ,byte_length_t index){
+  TM2x_F_PREFIX void *TM2x_at_index_pt(TM2x *dap ,address_t index){
     return dap->byte_0_pt + TM2x_element_byte_length(dap) * index;
   }
 
-  TM2x_F_PREFIX bool TM2x_read(TM2x *dap ,byte_length_t index ,void *dst_element_pt){
+  TM2x_F_PREFIX bool TM2x_read(TM2x *dap ,address_t index ,void *dst_element_pt){
     char *src_element_pt = TM2x_at_index_pt(dap ,index);
     if( src_element_pt >= dap->byte_np1_pt ) return false;
     memcpy(dst_element_pt, src_element_pt, TM2x_element_byte_length(dap));
@@ -176,7 +278,7 @@
   }
   #define TM2x_Read(dap ,index ,x) assert(TM2x_read ,index ,&x))
 
-  TM2x_F_PREFIX bool TM2x_write(TM2x *dap ,byte_length_t index ,void *src_element_pt){
+  TM2x_F_PREFIX bool TM2x_write(TM2x *dap ,address_t index ,void *src_element_pt){
     char *dst_element_pt = TM2x_at_index_pt(dap ,index);
     if( dst_element_pt >= dap->byte_np1_pt ) return false;
     memcpy(dst_element_pt, src_element_pt, TM2x_element_byte_length(dap));
