4a9acd73e239af99476307c33f4ba824dbe3af00
Thomas Walker Lynch <thomas.lynch@reasoningtechnology.com>
2020-08-01 20:45:51 +0200

    WIP

--- test/Inclusive·test.c
+++ test/Inclusive·test.c
@@ -1,13 +1,20 @@
+/*
+Good sign, the optimizer appears to eliminate all of the computation, the only 
+thing left is the call to print the passed message:
+gcc -s -std=gnu2x -Wall -O3 -I../include -L../lib  -o Inclusive·test.s Inclusive·test.c 
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "Result.h"
+//#include "Result.h"
 #include "Conveyance.h"
 #include "Inclusive.h"
 
 int main(){
+
   union{
     #include "Inclusive·Args.h"
   }Args;
@@ -17,20 +24,19 @@ int main(){
   }Locals;
 
   //-------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------
 
   uint64_t r;
 
   #pragma push_macro("S1")
   #undef S1
   #define S1 Args.Inclusive·3op
-
   S1.a0 = 541;
   S1.a1 = 727;
   S1.rpt = &r;
   S1.nominal = &&nominal;
   S1.gt_address_t_n = &&gt_address_t_n;
   continue_from Inclusive·mul_ib;
-
   #pragma pop_macro("S1") 
 
   nominal:{
@@ -38,7 +44,7 @@ int main(){
       printf("passed");
       exit(0);
     }
-    printf("failed");
+    printf("failed bad result");
     exit(1);
     cend;
   }
@@ -47,6 +53,13 @@ int main(){
     exit(1);
     cend;
   }
+
+  exraneous:{
+    printf("here!");
+  }
+
+  //-------------------------------------------------------------------------------------
+  //-------------------------------------------------------------------------------------
 
   #include "Inclusive·text.h"
 
