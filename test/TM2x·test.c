a8c8d2106e2d2a1fc897a77a23420e7a397dad39
Thomas Walker Lynch <thomas.lynch@reasoningtechnology.com>
2020-08-09 22:53:38 +0200

    multiple tests

--- test/TM2x·test.c
+++ test/TM2x·test.c
@@ -19,7 +19,7 @@ int main(){
   #include "CLib·DataTypes.h"
   #include "Inclusive·DataTypes.h"
   #include "TM2x·DataTypes.h"
-  struct test_00{
+  struct test_args0{
     ConveyancePtr continuation;
   };
 
@@ -27,7 +27,7 @@ int main(){
     #include "CLib·Data.h"
     #include "Inclusive·Data.h"
     #include "TM2x·Data.h"
-    struct test_00 test_0;
+    struct test_args0 test_args;
   };
   union Conveyance·Data Conveyance·Data0 ,Conveyance·Data1;
 
@@ -56,15 +56,21 @@ int main(){
   accumulated_results_pt=&accumulated_results;
   Result·Tallies·init(accumulated_results_pt);
 
-  AR(ar ,test_0 ,0);
-  ar->continuation = &&tests_finished;
-  continue_from test_0;
+  ConveyancePtr tests[] = {&&test_0 ,&&test_1 ,&&tests_finished};
+  ConveyancePtr *test_pt = tests;
+
+  test_commutator:{
+    AR(ar ,test_args ,0);
+    ar->continuation = &&test_commutator;
+    test_pt++;
+    continue_from **(test_pt - 1);
+  }
 
   test_0:{
     Conveyance nominal ,fail ,cleanup ,report;
 
     Conveyance·swap();
-    LC(lc ,test_0, 0);
+    LC(lc ,test_args, 0);
     CX(cx ,TM2x·Test ,test_0);
     cx->malloc_cnt = MallocCounter·count;
     cx->constructed_cnt = TM2x·constructed_count;
@@ -118,6 +124,72 @@ int main(){
       cx->f[cx->i++] = cx->malloc_cnt == MallocCounter·count;
       cx->f[cx->i++] = cx->constructed_cnt == TM2x·constructed_count;
       Result·Tallies·tally("test_0" ,&cx->results ,cx->f ,cx->i);
+      Result·Tallies·accumulate(accumulated_results_pt ,&cx->results);
+      continue_from *cx->continuation;
+      cend;
+    }
+
+    cend;
+  }
+
+  test_1:{
+    Conveyance nominal ,fail ,cleanup ,report;
+
+    Conveyance·swap();
+    LC(lc ,test_args, 0);
+    CX(cx ,TM2x·Test ,test_0);
+    cx->malloc_cnt = MallocCounter·count;
+    cx->constructed_cnt = TM2x·constructed_count;
+    cx->i = 0;
+    cx->continuation = lc->continuation;
+    Result·Tallies·init(&cx->results);
+
+    //allocate
+    AR(ar ,TM2x·alloc_heap ,0);
+    ar->tape = &cx->tape;
+    ar->nominal = &&nominal;
+    ar->fail = &&fail;
+    continue_from TM2x·alloc_heap;
+
+    nominal:{
+      Conveyance nominal;
+      cx->f[cx->i++] = true;
+
+      //construct
+      AR(ar ,TM2x·construct_bytes ,0);
+      ar->tape = cx->tape;
+      ar->byte_n = 48;
+      ar->nominal = &&nominal;
+      ar->alloc_fail = &&fail;
+      continue_from TM2x·construct_bytes;
+
+      nominal:{
+        cx->f[cx->i++] = Test·CLib·allocation_n == 63;
+        continue_from cleanup;
+        cend;
+      }
+      cend;
+    }
+
+    fail:{
+      cx->f[cx->i++] = false;
+      continue_from cleanup;
+      cend;
+    }
+
+    cleanup:{
+      // destruct and deallocate
+      AR(ar ,TM2x·destruct_dealloc_heap ,0);
+      ar->tape = cx->tape;
+      ar->nominal = &&report;
+      continue_from TM2x·destruct_dealloc_heap;
+      cend;
+    }
+
+    report:{
+      cx->f[cx->i++] = cx->malloc_cnt == MallocCounter·count;
+      cx->f[cx->i++] = cx->constructed_cnt == TM2x·constructed_count;
+      Result·Tallies·tally("test_1" ,&cx->results ,cx->f ,cx->i);
       Result·Tallies·accumulate(accumulated_results_pt ,&cx->results);
       continue_from *cx->continuation;
       cend;
