61c0a12e7dab8165dae87115f2b166f31995f7ff
Thomas Walker Lynch <thomas.lynch@reasoningtechnology.com>
2020-10-18 21:25:05 +0200

    deletes a couple of temp files

--- temp.sh
+++
@@ -1,5 +0,0 @@
-
-sed_tree . -e 's/copy_contiguous_bytes/copy_contiguous_bytes/g'
-sed_tree . -e 's/CopyContiguous/CopyContiguous/g'
-sed_tree . -e 's/copy_contiguous_elements/copy_contiguous_elements/g'
-sed_tree . -e 's/CopyContiguousElements/CopyContiguousElements/g'
